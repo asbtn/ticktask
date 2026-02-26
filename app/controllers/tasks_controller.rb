6f31514d155fd54ece70ede7b81f935f99765a3e
Alisa Subotina <alisa.sbtn@proton.me>
2026-02-26 17:35:56 +0200

    feat: basic working task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,40 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: %i[update destroy]
+
   def index
     @tasks = Current.user.tasks.all
+  end
+
+  def create
+    Current.user.tasks.create(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def update
+    @task.update!(task_params)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to tasks_path }
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Current.user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.expect(task: %i[completed description])
   end
 
 end
--- db/migrate/20260224200355_create_tasks.rb
+++ db/migrate/20260224200355_create_tasks.rb
@@ -2,9 +2,8 @@ class CreateTasks < ActiveRecord::Migration[8.1]
 
   def change
     create_table :tasks do |t|
-      t.string     :title
+      t.string     :description
       t.belongs_to :user
-      t.datetime   :due_at
       t.boolean    :completed, null: false, default: false
 
       t.timestamps
